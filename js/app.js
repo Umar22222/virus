d6b082b93b7af70be774d36928b8b05037d86468
Umar22222 <asadovumar222@gmail.com>
2024-08-31 18:37:16 +0500

    .

---
+++ js/app.js
@@ -0,0 +1,21 @@
+{
+    // 1 - misol
+    // let k = +prompt("sonni kiriting")
+    // let n = +prompt("sonni kiriting")
+
+    // for(let i = 0; i < n; i++){
+    //     console.log(k);
+        
+    // }
+}
+
+{
+    // 2 - misol
+    let a = +prompt("sonni kiriting")
+    let b = prompt("bu yerga true deb yozma")
+
+    for(a = a; a <= b; a++){
+        console.log(a);
+        
+    }
+}
